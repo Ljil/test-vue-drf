6690a5876d6e3a0ef33f80773e34e47189b6de28
ljil <randomlycoding@gmail.com>
2021-02-28 22:52:00 +0300

    добавлен permission для автора

--- backend/drfTest/posts/views.py
+++ backend/drfTest/posts/views.py
@@ -1,13 +1,14 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthor
 
 
 class PostsViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthor]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
