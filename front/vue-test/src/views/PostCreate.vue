382658fb127adec5fb8b1129befdf078a717fe27
ljil <randomlycoding@gmail.com>
2021-03-02 11:37:09 +0300

    Исправлено добавление поста

--- front/vue-test/src/views/PostCreate.vue
+++ front/vue-test/src/views/PostCreate.vue
@@ -2,11 +2,11 @@
   <div v-if="this.$store.state.token">
     <h1>Создание нового поста</h1>
     <p>
-      <label for="post_title">Заголовок</label>
+      <label for="post_title">Заголовок </label>
       <input id="post_title" v-model="title" placeholder="Заголовок"/>
     </p>
     <p>
-      <label for="post_text">Текст</label>
+      <label for="post_text">Текст </label>
       <input id="post_text" v-model="text" placeholder="Текст"/>
     </p>
     <button @click="create">Создать</button>
@@ -29,7 +29,7 @@ export default {
         title: this.title,
         text: this.text,
       }
-      console.log('Token ' + this.$store.getters.getAuthToken)
+      console.log('Token ' + this.$store.state.token)
       console.log(this.$store.state.baseUrl + 'api/posts/')
       console.log(JSON.stringify(data))
       fetch(this.$store.state.baseUrl + 'api/posts/', {
@@ -38,7 +38,7 @@ export default {
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
           'Accept': '*/*',
-          'Authorization': 'Token ' + this.$store.getters.getAuthToken
+          'Authorization': 'Token ' + this.$store.state.token
         },
         body: JSON.stringify(data)
       })
